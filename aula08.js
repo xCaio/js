c4265fca624c5c5919afad23ff7510ac096564b6
xcaio <caiosantos1703@gmail.com>
2025-06-30 22:15:41 -0300

    pre increment and post increment

---
+++ aula08.js
@@ -0,0 +1,8 @@
+let n1=11
+let n2=14
+
+//let res = n1 & n2 // bitwise AND
+let res = n1 | n2
+console.log(res)
+
+// aula de bitwise eu dropei, info paia 
---
+++ aula09.js
@@ -0,0 +1,16 @@
+let n=10
+
+
+//PRE INCREMENT
+console.log(++n) // 11
+
+
+//POS INCREMENT
+console.log(n++) // 11
+
+
+console.log(n) // 12
+
+console.log(n++) // 12
+
+console.log(n) // 13
