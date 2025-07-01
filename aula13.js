a76803582394e8190205eca19edc956e42179030
xcaio <caiosantos1703@gmail.com>
2025-07-01 19:56:29 -0300

    if, else if and else

---
+++ aula13.js
@@ -0,0 +1,16 @@
+let num1 = 90
+console.log("Edite o valor da variável do num1")
+if(num1 >= 10){
+    console.log("Numero maior ou igual a 10")
+}else if(num1 >= 3 && num1 <= 5){
+    console.log(`${num1} está entre  3 e 5`)
+}else if(num1 >= 6 && num1 <=9){
+    console.log(`${num1} está entre 6 e 9 dog`)
+}else if(num1 < 0){
+    console.log("Digite um número a partir do 0...")
+}
+
+else{
+    console.log("Ta entre 0 e 2 dog")
+}
+console.log("Fim do programa")
