0b00bb6477e9f2c51be3bda09171cedf533180cc
xcaio <caiosantos1703@gmail.com>
2025-07-01 06:52:35 -0300

    ternary operator

--- aula09.js
+++ aula09.js
@@ -1,5 +1,5 @@
 let n=10
-
+let n2 = 20
 
 //PRE INCREMENT
 console.log(++n) // 11
@@ -14,3 +14,7 @@ console.log(n) // 12
 console.log(n++) // 12
 
 console.log(n) // 13
+
+
+// concatenation
+console.log(n +""+ n2)
---
+++ aula10.js
@@ -0,0 +1,10 @@
+// Teste lógico ? se verdadeiro : se falso
+
+let idade = 18
+let podeDirigir = idade>= 18 ? "Pode Dirigir" : "Não pode Dirigir"
+console.log(podeDirigir)
+
+
+let num=5
+res=num % 2 == 0 ? "Par" : "Impar"
+console.log(res)
