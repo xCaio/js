cd63705a094c11723f1b92b88d5a46fc58752994
xcaio <caiosantos1703@gmail.com>
2025-07-01 20:38:14 -0300

    switch case

---
+++ aula14.js
@@ -0,0 +1,19 @@
+let lugar = 7;
+switch(lugar){
+    case 1:
+        console.log("Primeiro lugar");
+        break
+    case 2:
+        console.log("Segundo lugar");
+        break
+    case 3:
+        console.log("Terceiro lugar");
+        break
+    case 4: case 5: case 6:
+        console.log("Premio de participação");
+        break
+    default:
+        console.log("Não subiu ao pódio");
+        break
+}
+console.log("Fim do programa");
