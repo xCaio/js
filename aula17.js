b6779d70dcdfd1c09f3ee5f30a3c2d8efea1a3f8
xcaio <caiosantos1703@gmail.com>
2025-07-02 19:56:59 -0300

    while loop

---
+++ aula17.js
@@ -0,0 +1,17 @@
+// let i = 0
+// while (i <= 10) {
+//     console.log(i++)
+// }
+
+// console.log("Agr o i Ta grande fi");
+
+
+// 10! = 10*9*8*7*6*5*4*3*2*1 = 3.628.800
+let fatorial = 10
+let inicio = 1 
+
+while(fatorial > 0){
+    inicio *= fatorial
+    fatorial--
+    console.log(inicio)
+}
