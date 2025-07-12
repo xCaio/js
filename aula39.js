0bac889f7169be875bc63dfc6898e3a1e4b33536
xcaio <caiosantos1703@gmail.com>
2025-07-12 13:59:48 -0300

    relations of elements in the DOM #p2

---
+++ aula39.js
@@ -0,0 +1,28 @@
+const caixa1 = document.querySelector("#caixa1")
+const btn_c1 = document.querySelectorAll(".curso")
+const c1_2 = document.querySelector("#c1_2")
+
+// console.log(caixa1.hasChildNodes())
+// console.log(btn_c1[0].hasChildNodes())
+// console.log(btn_c1[0].childNodes)
+// console.log(btn_c1[0].children.length > 0 ? "Possui filhos" : "Não possui filhos")
+// console.log(caixa1.children.length > 0 ? "Possui filhos" : "Não possui filhos")
+
+// console.log(caixa1.children[1].innerHTML= "OK")
+
+
+console.log(c1_2.parentNode.parentNode.parentNode.children[4])
+
+
+
+
+
+
+// console.log(caixa1.children[caixa1.children.length-1])
+// console.log(caixa1.firstElementChild)
+// console.log(caixa1.lastElementChild)
+// console.log(caixa1.children)
+
+//NÓ RAIZ  -  ROOT NODE
+
+
