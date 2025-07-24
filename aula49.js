0c8f1bd5908eb1ddfb0ff7fc2d9d2fa03f0af32e
xcaio <caiosantos1703@gmail.com>
2025-07-24 20:51:11 -0300

    method some change

--- aula49.js
+++ aula49.js
@@ -3,14 +3,15 @@ const buscar = document.querySelector("#buscar")
 const resultado = document.querySelector("#resultado")
 
 
-const array = [11, 13, 15, 12, 25, 14, 16]
+const array = [11, 13, 15, 12, 9, 18, 16]
 p_array.innerHTML = "[" + array + "]"
 
 
 buscar.addEventListener('click', (event) => {
-    const retorno = array.some((element) => {
+    resultado.innerHTML = `Elemento não equivale as condições`
+    const retorno = array.some((element, index) => {
         if (element >= 18) {
-            resultado.innerHTML = `O valor ${element} é maior ou igual que 18`
+            resultado.innerHTML = `O valor ${element} é maior ou igual que 18 na posição ${index+1}`
         }
         return element >= 18
     })
