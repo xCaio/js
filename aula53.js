ab73232a29b7092175981a8254a1c14815f36002
xcaio <caiosantos1703@gmail.com>
2025-07-29 17:43:59 -0300

    array calculator (not finished)

--- aula53.js
+++ aula53.js
@@ -1,24 +1,43 @@
+ const valor1 = document.querySelector("#valor1")
+ const valor2 = document.querySelector("#valor2")
+ const resultado = document.querySelector("#resultado")
+ const btnSoma = document.querySelector("#soma")
+ const btnSubtracao = document.querySelector("#subtracao")
+ 
  let valores = [1,2,3,4]
  const op = [
-     (val)=>{
-        res = 0
-        for(i of val){
-            res+=i
-        }
-        return res 
+     (valor1, valor2)=>{
+        let res = Number(valor1 + valor2)
+        return res
     }, 
-    (val)=>{
-        res = 1
-        for(i of val){
-            res*=i
-        }
+    (valor1, valor2)=>{
+        res = valor1 - valor2
         return res
     },
-    (val)=>{
-        for(v of val){
-            console.log(v)
-        }
+    (valor1, valor2)=>{
+        res = valor1 * valor2
+        return res
+    },
+    (valor1,valor2)=>{
+        res = valor1/valor2
+        return res
     }
  ]
 
- console.log(op[2](valores))
+btnSoma.addEventListener('click', (event)=>{
+    let soma = op[0](Number(valor1.value),Number(valor2.value))
+    resultado.innerHTML = soma
+    valor1.value = ""
+    valor2.value = ""
+})
+btnSubtracao.addEventListener('click', (event)=>{
+    let subtracao = op[1](Number(valor1.value), Number(valor2.value))
+    resultado.innerHTML = subtracao
+    valor1.value = ""
+    valor2.value = ""
+})
+
+
+
+
+//  console.log(op[2](valores))
