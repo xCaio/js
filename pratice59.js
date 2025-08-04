ce39af0acadb1de415196e2ff9f6e40392a3e57e
xcaio <caiosantos1703@gmail.com>
2025-08-04 15:50:13 -0300

    manipulating HTML using OOP(pratice)

---
+++ pratice59.js
@@ -0,0 +1,41 @@
+class Pessoa {
+    constructor(pnome, pidade) {
+        this.nome = pnome
+        this.idade = pidade
+    }
+    getNome = () => this.nome
+    getIdade = () => this.idade
+
+    setNome(nome) {
+        this.nome = nome
+    }
+    setIdade(idade) {
+        this.idade = idade
+    }
+    info() {
+        console.log(`Nome: ${this.nome}\nIdade: ${this.idade}\n`)
+    }
+}
+
+const btn_add = document.querySelector("#btn_add")
+const resultado = document.querySelector(".res")
+let pessoas = []
+
+const addPerson = () => {
+    pessoas.map((element) => {
+        const div = document.createElement("div")
+        div.setAttribute("class", "pessoa")
+        div.innerHTML = `Nome: ${element.getNome()}<br/>Idade: ${element.getIdade()}`
+        resultado.appendChild(div)
+    })
+}
+
+
+btn_add.addEventListener('click', (event) => {
+    resultado.innerHTML=""
+    const nome = document.querySelector("#f_nome")
+    const idade = document.querySelector("#f_idade")
+    const p = new Pessoa(nome.value, idade.value)
+    pessoas.push(p)
+    addPerson()
+})
