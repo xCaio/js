e7e3704bdcc1dbf35bfd6f62d9977f2809d800f1
xcaio <caiosantos1703@gmail.com>
2025-08-08 10:51:42 -0300

    understanding static member

---
+++ aula66.js
@@ -0,0 +1,21 @@
+class Npc{
+    static alerta = false
+    constructor(energia){
+        this.energia = energia
+    }
+    info(){
+        console.log(`Energia: ${this.energia}\nAlerta: ${Npc.alerta?"Sim":"Não"}\n____________`)
+    }
+    static alertar = () =>{
+        this.alerta = true
+    }
+}
+
+const npc1 = new Npc(100)
+const npc2 = new Npc(80)
+
+Npc.alertar()
+
+npc1.info()
+npc2.info()
+npc2.info()
